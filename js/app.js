714064bdcf943b0a8622d85856bea6bca4b3a3d9
Mayank sharmas <51985173+sharmamayank221@users.noreply.github.com>
2020-10-15 10:27:34 +0545

    fixed animation

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+const navBanner = document.getElementById('nav');
+
 const navSlide = () => {
   const burger = document.querySelector(".burger");
   const nav = document.querySelector(".navbar .main-links");
@@ -18,4 +20,21 @@ const navSlide = () => {
   });
 };
 
+const hideNavBanner = () => {
+  setInterval(() => {
+navBanner.hidden = true;
+  }, 900)
+}
+
+// On scroll event listener for hiding navbar
+window.addEventListener('scroll', () => {
+  if(window.scrollY > 50) {
+    navBanner.classList.add("animate")
+    hideNavBanner();
+  }
+
+})
+
 navSlide();
+
+
